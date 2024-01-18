029f18468f82993682c4d568ca3a20971ef2b16f
plus-agc-service-yamauchi <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2024-01-18 11:45:18 +0900

    modified

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_98de2152.mjs';
+import { manifest } from './manifest_38abc777.mjs';
 import 'cookie';
 import 'kleur/colors';
 import 'string-width';
@@ -12,19 +12,24 @@ import 'mime';
 import 'path-to-regexp';
 
 const _page0  = () => import('./chunks/generic_0485acf7.mjs');
-const _page1  = () => import('./chunks/index_2752af48.mjs');
-const _page2  = () => import('./chunks/e-learning_4cf6b0e2.mjs');
-const _page3  = () => import('./chunks/calender_7fdc0a15.mjs');
-const _page4  = () => import('./chunks/greeting_52baa18b.mjs');
-const _page5  = () => import('./chunks/contact_676fae36.mjs');
-const _page6  = () => import('./chunks/cource-1_46eece6e.mjs');
-const _page7  = () => import('./chunks/cource-2_62ab3ec2.mjs');
-const _page8  = () => import('./chunks/cource-3_13e722fc.mjs');
-const _page9  = () => import('./chunks/cource_38203b4b.mjs');
-const _page10  = () => import('./chunks/event-20211212_10b97f37.mjs');
-const _page11  = () => import('./chunks/event-20220116_be035692.mjs');
-const _page12  = () => import('./chunks/event-20221204_6099dec2.mjs');
-const _page13  = () => import('./chunks/events_329a8d33.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/e-learning.astro", _page2],["src/pages/calender.astro", _page3],["src/pages/greeting.astro", _page4],["src/pages/contact.astro", _page5],["src/pages/cource/cource-1.astro", _page6],["src/pages/cource/cource-2.astro", _page7],["src/pages/cource/cource-3.astro", _page8],["src/pages/cource.astro", _page9],["src/pages/events/event-20211212.astro", _page10],["src/pages/events/event-20220116.astro", _page11],["src/pages/events/event-20221204.astro", _page12],["src/pages/events.astro", _page13]]);
+const _page1  = () => import('./chunks/index_5bcaab5f.mjs');
+const _page2  = () => import('./chunks/e-learning_f235e39c.mjs');
+const _page3  = () => import('./chunks/calender_c767e1a9.mjs');
+const _page4  = () => import('./chunks/greeting_33ee27e4.mjs');
+const _page5  = () => import('./chunks/contact_6da41e09.mjs');
+const _page6  = () => import('./chunks/cource-1_2574ba40.mjs');
+const _page7  = () => import('./chunks/cource-2_959056ed.mjs');
+const _page8  = () => import('./chunks/cource-3_140e4cf5.mjs');
+const _page9  = () => import('./chunks/cource_dadd742e.mjs');
+const _page10  = () => import('./chunks/event-20211212_ec3500d2.mjs');
+const _page11  = () => import('./chunks/event-20220116_fc43bae4.mjs');
+const _page12  = () => import('./chunks/event-20221204_86f7692d.mjs');
+const _page13  = () => import('./chunks/event-20230910_8355cbf1.mjs');
+const _page14  = () => import('./chunks/event-20231203_7b7bb309.mjs');
+const _page15  = () => import('./chunks/events_9053c5f0.mjs');
+const _page16  = () => import('./chunks/report-20230910_bef79985.mjs');
+const _page17  = () => import('./chunks/report-20231203_6c8ed9a7.mjs');
+const _page18  = () => import('./chunks/about_64c70cfc.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/e-learning.astro", _page2],["src/pages/calender.astro", _page3],["src/pages/greeting.astro", _page4],["src/pages/contact.astro", _page5],["src/pages/cource/cource-1.astro", _page6],["src/pages/cource/cource-2.astro", _page7],["src/pages/cource/cource-3.astro", _page8],["src/pages/cource.astro", _page9],["src/pages/events/event-20211212.astro", _page10],["src/pages/events/event-20220116.astro", _page11],["src/pages/events/event-20221204.astro", _page12],["src/pages/events/event-20230910.astro", _page13],["src/pages/events/event-20231203.astro", _page14],["src/pages/events.astro", _page15],["src/pages/report/report-20230910.astro", _page16],["src/pages/report/report-20231203.astro", _page17],["src/pages/about.astro", _page18]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
