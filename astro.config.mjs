a457824560845609260cf2216daf6b9e0687ef02
plus-agc-service-yamauchi <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2024-01-18 11:49:07 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,1 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
-export default defineConfig({
-    output: 'server',
-    adapter: netlify(),
-});
